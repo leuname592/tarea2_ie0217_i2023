f39488f0a8994e0eae1b293fe3ca10f48a3946a7
Emmanuel Chavarría Solís <leuname592@hotmail.com>
2023-05-06 14:48:39 -0600

    Fixing document structure

---
+++ tree/tarea2/Algoritmos/headerSort.hpp
@@ -0,0 +1,81 @@
+/**
+Licencia MIT
+
+Copyright (c) [2023] Tarea 2 - Grupo 2 - IE 0217 - I Ciclo 2023
+
+Se concede aquí, sin cargo, a cualquier persona que obtenga una copia de este
+software y los archivos de documentación asociados (el "Software"), para
+utilizar el Software sin restricciones, incluyendo, sin limitación, los
+derechos para usar, copiar, modificar, fusionar, publicar, distribuir,
+sublicenciar y/o vender copias del Software, y para permitir a las personas a
+quienes se les proporcione el Software que lo hagan, sujeto a las siguientes
+condiciones:
+
+El aviso de copyright anterior y este aviso de permiso se incluirán en todas
+las copias o partes sustanciales del Software.
+
+EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
+IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A GARANTÍAS DE COMERCIALIZACIÓN,
+IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS
+AUTORES O TITULARES DEL COPYRIGHT SERÁN RESPONSABLES POR NINGUNA RECLAMACIÓN,
+DAÑO O OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O DE
+OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO U
+OTROS ACUERDOS EN EL SOFTWARE.
+*/
+
+#ifndef HEADER_SORT_HPP
+#define HEADER_SORT_HPP
+
+#include <iostream>
+#include <time.h>
+using std::cout;
+using std::endl;
+
+/**
+ * @brief This function is used to print the values of the array
+ * 
+ * @param arr Array of elements
+ */
+void print_array(int arr[]);
+
+/**
+ * @brief This function is used to shuffle the values of the array once they are sorted
+ * 
+ * @param arr Array of elements
+ */
+void shuffle_array(int arr[]);
+
+/**
+ * @brief Bubble sort the values of the array iterating through it
+ * 
+ * @param arr Array of elements
+ * @param n Size of the array
+ */
+void bubbleSort(int arr[], int n);
+
+/**
+ * @brief Selection sort the values of the array iterating through it
+ * 
+ * @param arr Array of elements
+ * @param n Size of the array
+ */
+void selectionSort(int arr[], int n);
+
+/**
+ * @brief Insertion sorting the values of the array iterating through it
+ * 
+ * @param arr Array of elements
+ * @param n Size of the array
+ */
+void insertionSort(int arr[], int n);
+
+/**
+ * @brief Quicksort the values of the array iterating through it using recursion
+ * 
+ * @param arr Array of elements
+ * @param low First element index in the array
+ * @param high Last element index in the array
+ */
+void quickSort(int arr[], int low, int high);
+
+#endif /* HEADER_SORT_HPP */
---
+++ tree/tarea2/Templates/README.md
@@ -0,0 +1,13 @@
+##Templates section
+
+- [x] Cleaning Includes
+```cpp
+#include <stdexcept>
+```
+- [x] Comment code using Doxygen
+
+- [x] Comment main
+
+- [x] Adding Float implementation
+
+- [x] Adding Out of range trigger as a comment
